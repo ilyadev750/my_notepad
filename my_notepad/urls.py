2416dafa15da7960b82d99c5c0ce11355620d447
ilyadev750 <petrovich76007600kz@gmail.com>
2023-05-14 15:07:33 +0300

    changing passwords working

--- my_notepad/urls.py
+++ my_notepad/urls.py
@@ -3,14 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from note.views import anonymous_note, start, new_user_note
-from django.views.decorators.cache import cache_page
+from note.views import anonymous_note, start
 
 
 urlpatterns = [
     path("", start, name="start"),
     path("base/", anonymous_note, name="anonymous_note"),
-    path("admin/", admin.site.urls),
-    path("base/users/", include("django.contrib.auth.urls")),
     path("base/users/", include("users.urls")),
+    path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,40 @@
-from django.urls import path, re_path
-from .views import login_user, logout_user, register_user, reset_password, set_password
+from django.urls import path
+from .views import login_user, logout_user, register_user
 from note.views import new_user_note, get_user_notes, update_user_note, delete_user_note
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("login_user/", login_user, name="login"),
     path("logout_user/", logout_user, name="logout"),
     path("register_user/", register_user, name="register"),
-    # path("reset_password/", reset_password, name="reset_password"),
-    # path("set_password/", set_password, name="set_password"),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="password/password_reset.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="password/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="password/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="password/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
     path("<int:user_id>/", get_user_notes, name="get_user_notes"),
     path(
         "<int:user_id>/<str:type_of_note>/create/", new_user_note, name="new_user_note"
