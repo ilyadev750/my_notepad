25f47f6f5246e4c0e4cf8192e5bdd1f69b344082
ilyadev750 <petrovich76007600kz@gmail.com>
2023-06-02 21:03:35 +0300

    I understand how to put django form into css

--- note/forms.py
+++ note/forms.py
@@ -2,41 +2,45 @@ from .models import Note
 from django import forms
 from django.utils import timezone
 
+
 class AnonymousNoteForm(forms.Form):
-    title = forms.CharField(max_length=255, required=False)
-    content = forms.CharField(widget=forms.Textarea(attrs={
-        "class": "note-content",
-        "placeholder": "",
-        "name": "content",
-        "rows": 10,
-        "cols": 10,
-    }), required=False)
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        widget=forms.TextInput(attrs={"class": "editor-container"}),
+    )
+    content = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "editor-container"}), required=False
+    )
     create = forms.DateTimeField(widget=forms.HiddenInput())
     update = forms.DateTimeField(widget=forms.HiddenInput())
     username = forms.CharField(widget=forms.HiddenInput())
 
 
 class UserCreateNoteForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(attrs={
-        "class": "note-content",
-        "placeholder": "",
-        "name": "content",
-        "rows": 10,
-        "cols": 10,
-    }))
+    title = forms.CharField(
+        max_length=255, widget=forms.TextInput(attrs={"class": "editor-container"})
+    )
+    content = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "editor-container"})
+    )
     create = forms.DateTimeField(widget=forms.HiddenInput())
     update = forms.DateTimeField(widget=forms.HiddenInput())
     username = forms.CharField(widget=forms.HiddenInput())
 
-class UserUpdateNoteForm(forms.Form):
-    title = forms.CharField(max_length=255, error_messages={'required': ''})
-    content = forms.CharField(widget=forms.Textarea(attrs={
-        "class": "note-content",
-        "placeholder": "",
-        "name": "content",
-        "rows": 10,
-        "cols": 10,
-    }), error_messages={'required': ''})
-    update = forms.DateTimeField(widget=forms.HiddenInput())
 
+class UserUpdateNoteForm(forms.Form):
+    title = forms.CharField(max_length=255, error_messages={"required": ""})
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "note-content",
+                "placeholder": "",
+                "name": "content",
+                "rows": 10,
+                "cols": 10,
+            }
+        ),
+        error_messages={"required": ""},
+    )
+    update = forms.DateTimeField(widget=forms.HiddenInput())
