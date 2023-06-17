9ec269468bc94d0296fa176458ef6bc63b576196
ilyadev750 <petrovich76007600kz@gmail.com>
2023-06-17 14:35:00 +0300

    make fields in the center

--- note/forms.py
+++ note/forms.py
@@ -7,7 +7,7 @@ class AnonymousNoteForm(forms.Form):
     title = forms.CharField(
         max_length=255,
         required=False,
-        widget=forms.TextInput(attrs={"class": "editor-container"}),
+        widget=forms.TextInput(attrs={"class": "form-content-title"}),
     )
     content = forms.CharField(
         widget=forms.Textarea(attrs={"class": "form-content"}),
