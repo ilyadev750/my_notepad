333ebf36d016f129c768725006c1e9ba13ac7f00
ilyadev750 <petrovich76007600kz@gmail.com>
2024-01-03 20:09:44 +0100

    my project is alive!

--- article/models.py
+++ article/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from ckeditor.fields import RichTextField
 
 
 
-class Article(models.Model):
-    title = models.CharField(max_length=255)
-    content = RichTextField(blank=True, null=True)
+
+# class Article(models.Model):
+#     title = models.CharField(max_length=255)
+#     content = RichTextField(blank=True, null=True)
--- my_notepad/settings.py
+++ my_notepad/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "ckeditor",
-    "ckeditor_uploader",
-    "article",
+    # "ckeditor",
+    # "ckeditor_uploader",
+    # "article",
     "note",
     "users",
 ]
