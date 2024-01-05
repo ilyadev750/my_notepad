2a91e2778f929b7a1109667fb60a12be03c938d1
ilyadev750 <petrovich76007600kz@gmail.com>
2024-01-05 03:01:20 +0100

    add absolutely new templates

--- my_notepad/urls.py
+++ my_notepad/urls.py
@@ -3,13 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from note.views import anonymous_note, start, about
+from main import views
+from note.views import anonymous_note, about
 
 
 urlpatterns = [
-    path("", start, name="home"),
+    path("", views.index, name="index"),
     path("about/", about, name="about"),
     path("base/", anonymous_note, name="anonymous_note"),
     path("base/users/", include("users.urls")),
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
